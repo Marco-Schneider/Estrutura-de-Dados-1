89699daff6b2927211aa243239bcaa85832bdcee
Marco-Schneider <marco.schneider452@gmail.com>
2023-03-20 00:58:32 -0300

    Initial commit

---
+++ Lista 2/Arquivos/par_impar.c
@@ -0,0 +1,10 @@
+#include "queue.h"
+
+int main () {
+  int i, n = 10;	
+  /*Use somente as operações definidas em queue.h*/
+  /*Não use para resolver esse exercício o operador -> aqui!*/
+  /*Terminar*/
+  return 0;
+}
+

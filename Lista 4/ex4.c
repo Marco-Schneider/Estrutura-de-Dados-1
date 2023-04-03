c0bda061e9d5a3ccd8e33e10b53ca88403001222
Marco-Schneider <marco.schneider452@gmail.com>
2023-04-03 20:18:58 -0300

    Completed exercise 4

---
+++ Lista 4/ex4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Arquivos/list.h"
+
+/* This function receives a list and returns its number of elements */
+int size(List *l){
+
+  List *t;
+  int numberOfElements = 0;
+
+  for(t=l; t!=NULL; t=t->next) {
+    numberOfElements += 1;
+  }
+
+  return numberOfElements;
+}
+
+int main() {
+
+  int tam = 10;
+  List *l = NULL;
+
+  for (int i = 0; i < tam; i++) 
+  l = insert (l, i);
+
+  print(l);
+  printf("A lista possui %d elementos!\n", size(l));
+
+  return 0;
+}

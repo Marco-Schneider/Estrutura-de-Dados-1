90f84a751c23589f90d715716359ba1806214b00
marco-schneider <marco.schneider452@gmail.com>
2023-04-04 15:23:23 -0300

    Initial implementation of the exercise 6

---
+++ Lista 5/ex6.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Arquivos/list.h"
+
+
+int max(List *l) {
+
+  List *head;
+  List *helper;
+
+  head = l;
+  helper = head;
+
+  if(l != NULL) {
+    if(head->data > helper-> data) {
+      return head->data;
+    }
+    return max(l);
+  }
+
+}
+
+int main() {
+
+  List *l = NULL;
+  int k;
+
+  for (k = 5; k <= 9; k++) {
+    l = insert_back (l, k);
+  }
+  for (k = 4; k >= 0; k--) {
+    l = insert_back (l, k);
+  }
+
+  printf("The highest number on the list is: %d\n", max(l));
+
+  return 0;
+}

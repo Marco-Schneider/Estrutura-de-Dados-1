bff75eddbd4fc0228fbd11c6ca2dc605de08e6c3
marco-schneider <marco.schneider452@gmail.com>
2023-05-23 15:17:15 -0300

    Completed exercise 1

--- Lista 10/Arquivos/merge-sort.c
+++ Lista 10/Arquivos/merge-sort.c
@@ -5,15 +5,37 @@
  {m} é o meio (ou middle), {r} é a direita (de right),
  {O} é o vetor auxilar (de output). */
 void merge (int A[], int l, int m, int r, int O[]) {
-  /*Terminar*/	
+  int i = l;
+  int j = m + 1;
+  int k = l;
+  while((i<=m) && (j<=r)) {
+    if(A[i] <= A[j]) {
+      O[k++] = A[i++];
+    } else {
+      O[k++] = A[j++];
+    }
+  }
+  while(i <= m) {
+    O[k++] = A[i++];
+  }
+  while(j <= r) {
+    O[k++] = A[j++];
+  }
+  for(j=l; j <= r; j++) {
+    A[j] = O[j];
+  }
 }
 
 /*Função principal do algoritmo Merge-Sort.*/
 void merge_sort (int A[], int l, int r, int O[]) {
-  /*Terminar*/	
+  if(l < r) {
+    int m = (l + r)/2;
+    merge_sort(A, l, m, O);
+    merge_sort(A, m+1, r, O);
+    merge(A, l, m, r, O);
+  }
 }
 
-/* */
 int main (int argc, char *argv[]) {
   clock_t start, end;
   double elapsed_time;
@@ -27,15 +49,15 @@ int main (int argc, char *argv[]) {
   int *A = (int *)malloc(n * sizeof(int));
   int *O = (int *)malloc(n * sizeof(int)); /*array auxiliar*/
   for (i = 0; i < n; i++) {
-    A[i] = rand() % (n+1); /*valores aleatórios*/
-    //A[i] = i;   /*valores em ordem crescente*/
+    // A[i] = rand() % (n+1); /*valores aleatórios*/
+    A[i] = i;   /*valores em ordem crescente*/
     //A[i] = n-i; /*valores em ordem descrescente*/
     //A[i] = 0;   /*valores iguais*/
   }
   start = clock();
-  print (A, n, "Input");
+  // print (A, n, "Input");
   merge_sort (A, 0, n-1, O);
-  print (A, n, "Sorted");
+  // print (A, n, "Sorted");
   end = clock();
   elapsed_time = (end - start)/(double)CLOCKS_PER_SEC;
   printf("Running time: %.2f\n", elapsed_time);

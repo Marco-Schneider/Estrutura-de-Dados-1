e637e896e0b8def4a828d1656ccffe78389c40ed
Marco-Schneider <marco.schneider452@gmail.com>
2023-06-05 19:26:59 -0300

    Concluding exercise 3

--- Lista 11/Arquivos/counting-sort-char.c
+++ Lista 11/Arquivos/counting-sort-char.c
@@ -1,24 +1,33 @@
 #include "utils.h"
 
-/* */
 void counting_sort (FILE *f, int range) {
-  int i, j, total = 0;
-  int *C = (int *)malloc((range+1) * sizeof(int)); /*contador*/
-  /*Terminar*/
-  /*Imprimir no final desta função as frequências das letras!*/
-}
+  int i, j, c, total = 0;
+  int *C = (int *)malloc((range+1) * sizeof(int));
 
-/* */
-int main (int argc, char *argv[]) {
-
-  if (argc < 2) {
-    printf("run: %s file.txt\n", argv[0]);
-    exit(1);
+  for (i = 0; i <= range; i++) {
+    C[i] = 0;
   }
 
-  FILE *f = fopen(argv[1], "r");
+  while ((c = fgetc(f)) != EOF) {
+    if (c >= 'a' && c <= 'z') {
+      C[c - 'a']++;
+      total++;
+    }
+  }
 
-  int range = 25; /*número de letras do alfabeto (a-z)*/
+  for (i = 0; i <= range; i++) {
+    float frequency = (float) C[i] / total;
+    printf("%c:  %6d  %.4f\n", 'a' + i, C[i], frequency);
+  }
+
+  free(C);
+}
+
+int main (int argc, char *argv[]) {
+
+  FILE *f = fopen("livros/livro_pt.txt", "r");
+
+  int range = 25;
 
   counting_sort (f, range);
   

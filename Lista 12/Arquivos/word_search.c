e2238d12a4aa0843f716a3b13888d5ef151c5273
marco-schneider <marco.schneider452@gmail.com>
2023-06-06 13:44:28 -0300

    Adding linear search

--- Lista 12/Arquivos/word_search.c
+++ Lista 12/Arquivos/word_search.c
@@ -4,7 +4,11 @@
 
 /* */
 int linear_search (char **words, int n, char *key) {
-  /*Terminar*/
+  for(int i=0; i<n; i++) {
+    if(strcmp(words[i], key) == 0) {
+      return i;
+    }
+  }
 }
 
 /* */

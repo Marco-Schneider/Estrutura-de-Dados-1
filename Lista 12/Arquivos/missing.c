3a699bba0c033b8c70195c84bdd02c4f2c67f1a9
Marco-Schneider <marco.schneider452@gmail.com>
2023-06-12 20:33:41 -0300

    Completing exercise 2

--- Lista 12/Arquivos/missing.c
+++ Lista 12/Arquivos/missing.c
@@ -4,32 +4,35 @@
 int missing_number (int *A, int n, int m) {
   int left = 0, right = n-1;
 
-  while(left<=right) {
-    int middle = (left + right)/2;
-    if(A[middle] == middle) {
-      left = middle+1;
+  while (left <= right) {
+    int middle = (left + right) / 2;
+
+    if(A[middle] == middle) { // Check right
+      left = middle + 1;
     }
-    else if()
+    else { // Check left
+      right = middle - 1;
+    }
 
   }
-  return 0;
+  return left;
 } 
 
 /* */
 int main () {
   int i;
-  //int n = 4, m = 4;
-  //int A[] = {0,1,2,3};
+  // int n = 4, m = 4;
+  // int A[] = {0,1,2,3};
   // int n = 4, m = 12;
   // int A[] = {4,5,10,11};
-  int n = 7, m = 16;
-  int A[] = {0,1,2,6,9,11,15};
-  //int n = 8, m = 16;
-  //int A[] = {1,2,3,4,6,9,11,15};
+  // int n = 7, m = 16;
+  // int A[] = {0,1,2,6,9,11,15};
+  int n = 8, m = 16;
+  int A[] = {1,2,3,4,6,9,11,15};
   //int n = 7, m = 7;
   //int A[] = {0,1,2,3,4,5,6};
-  //int n = 8, m = 11;
-  //int A[] = {0,1,2,3,4,5,6,7,10};
+  // int n = 8, m = 11;
+  // int A[] = {0,1,2,3,4,5,6,7,10};
   print (A, n, "Input");
   printf("Missing number: %d\n", missing_number (A, n, m));
   return 0;

8b35f296caff625afe0f274bc75f7324c83d9733
Marco-Schneider <marco.schneider452@gmail.com>
2023-06-18 21:37:26 -0300

    Completing exercise 7

---
+++ Lista 8/ex7.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Arquivos/utils.h"
+
+void selection_sort_descending_order (int *A, int n) {
+  for (int i = 0; i < n - 1; i++) {
+    int highest = i;
+    for (int j = i + 1; j < n; j++) {
+      if (A[j] > A[highest]) {
+        highest = j;
+      }
+    }
+    if (highest != i) {
+      int temp = A[i];
+      A[i] = A[highest];
+      A[highest] = temp;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  clock_t start, end;
+  double elapsed_time;
+  srand(time(NULL));
+
+  if(argc < 2) {
+    printf("run: %s [size]\n", argv[0]);
+    exit(1);
+  }
+   
+  int i;
+  int n = atoi(argv[1]);
+  int range = 10; /*intervalo de valores sorteados*/
+  int *A = (int *)malloc(n * sizeof(int));
+
+  for(i = 0; i < n; i++) {
+    A[i] = rand() % (n+1); /*valores aleatórios*/
+  }  
+
+  start = clock();
+  print (A, n, "Input");
+  selection_sort_descending_order (A, n);
+  print (A, n, "Sorted");
+  end = clock();
+  elapsed_time = (end - start)/(double)CLOCKS_PER_SEC;
+  printf("Running time: %.2f\n", elapsed_time);
+  if(!check(A, n, FALSE)) {
+    printf("Error: non-descending sorted sequence!\n");
+  }
+  free (A);
+  return 0;
+}
